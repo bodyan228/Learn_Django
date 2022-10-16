2eed7fda7712d2a61288ca736142781127c16e3c
bodyan228 <vanya_bolshakov_02@mail.ru>
2022-10-16 23:58:11 +0500

    add_auth

--- first/firstsite/news/views.py
+++ first/firstsite/news/views.py
@@ -1,14 +1,58 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView, CreateView
 from .models import News, Category
-from .forms import NewsForm
-from django.urls import reverse_lazy
+from .forms import NewsForm, UserRegisterForm, UserLoginForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.paginator import Paginator
+from django.contrib import messages
+from django.contrib.auth import login, logout
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            messages.success(request, 'Вы успешно зарегистрировались!')
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Ошибка регистрации!')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'news/register.html', {'form': form})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserLoginForm()
+    return render(request, 'news/login.html', {'form': form})
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
+
+
+def test(request):
+    objects = ['jonh1', 'paul1', 'george3', 'asda4', 'ewrfwef5', 'sadfsd6', 'sdfsd7']
+    paginator = Paginator(objects, 2)
+    page_num = request.GET.get('page', 1)
+    page_objects = paginator.get_page(page_num)
+    return render(request, 'news/test.html', {'page_obj': page_objects})
 
 
 class HomeNews(ListView):
     model = News
     template_name = 'news/home_news_list.html'
     context_object_name = 'news'
+    paginate_by = 3
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -24,6 +68,7 @@ class NewsByCategory(ListView):
     template_name = 'news/home_news_list.html'
     context_object_name = 'news'
     allow_empty = False
+    paginate_by = 2
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -33,6 +78,18 @@ class NewsByCategory(ListView):
     def get_queryset(self):
         return News.objects.filter(category_id=self.kwargs['category_id'],
                                    is_published=True).select_related('category')
+
+
+class ViewNews(DetailView):
+    model = News
+    context_object_name = 'news_item'
+
+
+class CreateNews(LoginRequiredMixin, CreateView):
+    form_class = NewsForm
+    template_name = 'news/add_news.html'
+    # success_url = reverse_lazy('home')
+    login_url = '/admin/'
 
 
 # def index(request):
@@ -55,9 +112,7 @@ class NewsByCategory(ListView):
 #     news_item = get_object_or_404(News, pk=news_id)
 #     return render(request, 'news/view_news.html', {'news_item': news_item})
 
-class ViewNews(DetailView):
-    model = News
-    context_object_name = 'news_item'
+
     # template_name = 'news/news_detail.html'
     # pk_url_kwarg = 'news_id'
 
@@ -74,10 +129,7 @@ class ViewNews(DetailView):
 #     return render(request, 'news/add_news.html', {'form': form})
 
 
-class CreateNews(CreateView):
-    form_class = NewsForm
-    template_name = 'news/add_news.html'
-    # success_url = reverse_lazy('home')
+
 
 
 
